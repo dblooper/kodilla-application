cabbf452acec6994b9dece0f7529085306c96c7d
dkoplenski <dkoplenski@gmail.com>
2019-04-29 22:39:39 +0200

    Task i Trello Controller Tests - Submoduł 29.3

--- src/main/java/com/crud/tasks/service/Mail.java
+++ src/main/java/com/crud/tasks/service/Mail.java
@@ -1,14 +1,17 @@
 package com.crud.tasks.service;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Mail {
     private String mailTo;
     private String toCc;
     private String subject;
     private String message;
+
 }
